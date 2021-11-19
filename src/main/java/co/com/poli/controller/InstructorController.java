076d5800b6dba2ff4fdbcdd89bebc74e20529e1b
juan-pabloa <juanpablolopez36@gmail.com>
2021-11-19 18:42:58 -0500

    cambio en la coneccion a la bd

--- src/main/java/co/com/poli/controller/InstructorController.java
+++ src/main/java/co/com/poli/controller/InstructorController.java
@@ -3,6 +3,7 @@ package co.com.poli.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import co.com.poli.entities.Instructor;
 import co.com.poli.services.InstructorService;
 
 @RestController
+@CrossOrigin 
 @RequestMapping("/instructor")
 public class InstructorController {
 
